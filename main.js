35bf67b31650a729bab02434960eeb1a8bdd706a
sbaldwin970 <sdbj93@gmail.com>
2016-01-04 08:33:25 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+angular.module('siftSorter', []);
+var mainController = function($scope) {
+	$scope.findWord = "";
+	$scope.articleTxt = "";
+	$scope.sortResult = "";
+	$scope.sortTxt = "";
+	var sortHits = [];
+	$scope.siftResult = "";
+	$scope.siftTxt = "";
+	var siftHits = [];
+	var frequency = {};
+	// SORT HANDLER //
+	$scope.sortDisplay = function() {
+		$scope.sortTxt = $scope.articleTxt;
+		$scope.sortTxt = $scope.sortTxt.toLowerCase();
+		$scope.sortTxt = $scope.sortTxt.split(' ');
+		$scope.sortResult = sorter($scope.sortTxt);
+	}
+	var sorter = function(words) {
+		var frequency = {}
+		for(var j = 0; j < words.length; j++) {
+			temp = words[j]
+			frequency[temp] = frequency[temp] || 0
+			frequency[temp] +=1;
+		}
+		for(var freq in frequency)
+			sortHits.push([freq, frequency[freq]])
+			sortHits.sort(function(a, b) {
+				return b[1] - a[1]
+			})
+		var empty = "";
+		for(var k = 0; k < sortHits.length; k ++) {
+			empty = empty + sortHits[k][0] + ' was found ' + sortHits[k][1] + ' times '  
+			console.log(empty)
+		}
+		return empty  
+	}
+	// SIFT HANDLER //
+	$scope.siftDisplay = function() {
+		$scope.siftTxt = $scope.articleTxt;
+		$scope.siftTxt = $scope.siftTxt.toLowerCase();
+		$scope.siftTxt = $scope.siftTxt.split(' ');
+		$scope.siftResult = sift($scope.findWord);
+	}
+	var sift = function(word) {
+		for(var i = 0; i < $scope.siftTxt.length; i++) {
+			if($scope.siftTxt[i] === word) {
+				siftHits.push(word);
+			}
+		}
+		if(siftHits.length > 1) {
+			return "Found:" + " " + '"' + word + '"' + " " + siftHits.length + " times."; 
+		}
+		else if(siftHits.length === 1) {
+			return "Found:" + " " + '"' + word + '"' + " " + siftHits.length + " time.";
+		}
+		else {
+			return "No matches!!!"
+		}
+	};
+};
+
+angular.module('siftSorter')
+.controller('mainController', ['$scope', mainController]);
